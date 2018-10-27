e0d1fdf833c382a0d13e40285f96acad6d7d460d
Ignacio Chiazzo <ignaciochiazzo@gmail.com>
2018-10-27 13:03:20 -0400

    Added n queens problem

--- Main.js
+++ Main.js
@@ -2,9 +2,11 @@
 var permutationWithoutDuplicates = require("./PermutationsWithoutDuplicates.js");
 var permutationWithDuplicates = require("./PermutationsWithDuplicates.js");
 var subsets = require("./Subsets.js");
+var nQueens = require("./NQueens.js");
 
 // Invocation
 
 // permutationWithoutDuplicates.main();
 // permutationWithDuplicates.main();
 // subsets.main();
+// nQueens.main();
