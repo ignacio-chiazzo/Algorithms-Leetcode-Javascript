2822b619dd3ff0d7fb3a23b7e5cd22d7c92a7e87
Ignacio Chiazzo Cardarello <ignaciochiazzo@gmail.com>
2018-11-04 23:52:40 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -4,31 +4,30 @@ const PROBLEMS_FOLDER = './LeetcodeProblems/';
 const REGEX_PATTERN_HIDDEN_FILES = /(^|\/)\.[^\/\.]/g;
 
 var main = async function() {
-	try {
-		const problems = await loadProblems();
-
-		for(i in problems) { 
-			console.log("Solving: " + problems[i] + ":");
-			const problem = require(PROBLEMS_FOLDER + problems[i]);
-			problem.main();
-			console.log("End of the solution for : " + problems[i] + ",\n\n");
-		}
-	} catch (error) {
-		throw new Error(error);
-	}
+  try {
+    const problems = await loadProblems();
+    for(i in problems) { 
+      console.log("Solving: " + problems[i] + ":");
+      const problem = require(PROBLEMS_FOLDER + problems[i]);
+      problem.main();
+      console.log("End of the solution for : " + problems[i] + ",\n\n");
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 var loadProblems = () => {
-	return new Promise(function (resolve, reject) {
-		fs.readdir(PROBLEMS_FOLDER, (error, files) => {
-			if (error) {
+  return new Promise(function (resolve, reject) {
+    fs.readdir(PROBLEMS_FOLDER, (error, files) => {
+      if (error) {
         reject(error);
       } else {
-				problems = files.filter(item => !(REGEX_PATTERN_HIDDEN_FILES).test(item));
-			  resolve(problems);
+	problems = files.filter(item => !(REGEX_PATTERN_HIDDEN_FILES).test(item));
+	resolve(problems);
       }
-		})
-	});
+    })
+  });
 }
 
 main();
