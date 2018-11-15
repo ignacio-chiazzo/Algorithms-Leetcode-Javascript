86afaf562cdc67e99292b34e516912252bead0c4
Ignacio Chiazzo Cardarello <ignaciochiazzo@gmail.com>
2018-11-14 23:14:02 -0500

    Update Search-a-2D-Matrix-II.js

--- LeetcodeProblems/Search-a-2D-Matrix-II.js
+++ LeetcodeProblems/Search-a-2D-Matrix-II.js
@@ -1,9 +1,31 @@
+/*
+https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+ Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+ Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+ Consider the following matrix:
+ [
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+Given target = 20, return false.
+*/
+
+/**
+* @param {number[][]} matrix
+* @param {number} target
+* @return {boolean}
+*/
 var searchMatrix = function(matrix, target) {
   if (matrix.length == 0)
       return false
   var lastCol = matrix[0].length - 1;
   var firstRow = 0;
-  
   
   while(lastCol >= 0 && firstRow < matrix.length) {
       if(matrix[firstRow][lastCol] == target) {
@@ -17,6 +39,7 @@ var searchMatrix = function(matrix, target) {
   
   return false;
 };
+
 const matrix1 = [
   [1,4,7,    11,15],
   [2,5,8,    12,19],
@@ -24,7 +47,6 @@ const matrix1 = [
   [10,13,14, 17,24],
   [18,21,23, 26,30]
 ];
-
 
 var main = function(n) {
   console.log(searchMatrix(matrix1, 5));
