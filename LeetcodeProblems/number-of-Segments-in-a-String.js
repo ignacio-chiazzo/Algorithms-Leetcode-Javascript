b37adb0ccb6cb08790e7e02d56e5a8d4457122b2
Ignacio Chiazzo Cardarello <ignaciochiazzo@gmail.com>
2018-11-23 00:28:16 -0500

    Create number-of-Segments-in-a-String.js

---
+++ LeetcodeProblems/number-of-Segments-in-a-String.js
@@ -0,0 +1,47 @@
+/*
+
+434. Number of Segments in a String
+https://leetcode.com/problems/number-of-segments-in-a-string/description/
+
+Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+Please note that the string does not contain any non-printable characters.
+
+Example:
+
+Input: "Hello, my name is John"
+Output: 5
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+  var count = 0;
+  var i = 0;
+
+  while(i < s.length) {
+    if(s[i] !== " ") {
+      count++;
+      while(i < s.length && s[i] !== " ") {
+        i++;
+      }
+    }
+    i++;
+  }
+  
+  return count;
+};
+
+
+function main() {
+  console.log(countSegments(" "));
+  console.log(countSegments("    "));
+  console.log(countSegments("ab cd ef"));
+  console.log(countSegments("  ab cd ef"));
+  console.log(countSegments("ab cd ef  "));
+  console.log(countSegments(" ab cd ef "));
+}
+
+module.exports.main = main
