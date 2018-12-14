19b57215a72097a5b8cb0c4bf74e366b0a51767f
Ignacio Chiazzo <ignaciochiazzo@gmail.com>
2018-12-14 10:10:50 -0500

    Added 3Sum, Min Stack and SumOfSquare

---
+++ LeetcodeProblems/3Sum.js
@@ -0,0 +1,60 @@
+/*
+3Sum
+https://leetcode.com/problems/3sum/description/
+
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  var ret = [];
+  nums.sort(function(a, b) { return a - b });
+  for(var i = 0; i < nums.length; i++) {
+      if(i === 0 || i > 0 && nums[i] !== nums[i - 1]) {
+          var left = i + 1;
+          var right = nums.length - 1;
+          while(left < right) {
+              const sum = nums[left] + nums[right] + nums[i];
+              if(left > i + 1 && nums[left] === nums[left - 1] || sum < 0) {
+                  left++
+              } else if(right < nums.length - 1 && nums[right] === nums[right + 1] || sum > 0) {
+                  right--;
+              } else if(sum === 0) { 
+                  ret.push([nums[left], nums[right], nums[i]]);
+                  left++;
+                  right--;   
+              }
+          } 
+      }
+  }
+  
+  return ret;
+};
+
+var main = function() {
+  console.log(threeSum([]));
+  console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+  console.log(threeSum([0]));
+  console.log(threeSum([0, 0]));
+  console.log(threeSum([0, 0, 0]));
+  console.log(threeSum([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]));
+}
+
+module.exports.main = main;
---
+++ LeetcodeProblems/Sum_Of_Square_Numbers.js
@@ -0,0 +1,44 @@
+/*
+Sum of Square Numbers
+https://leetcode.com/problems/sum-of-square-numbers/description/
+
+Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
+
+Example 1:
+Input: 5
+Output: True
+Explanation: 1 * 1 + 2 * 2 = 5
+
+Example 2:
+Input: 3
+Output: False
+*/
+
+
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function(c) {
+  var iter = 0;
+  var set = new Set();
+  while(iter ** 2 <= c) {
+    var square = iter * iter;
+    if(square * 2 === c || set.has(c - square))
+      return true;
+
+    set.add(square);
+    iter++;
+  }
+  
+  return false;
+};
+
+var main = function() {
+  console.log(judgeSquareSum(0));
+  console.log(judgeSquareSum(1));
+  console.log(judgeSquareSum(5));
+  console.log(judgeSquareSum(16));
+  console.log(judgeSquareSum(24));
+  console.log(judgeSquareSum(25));
+}
