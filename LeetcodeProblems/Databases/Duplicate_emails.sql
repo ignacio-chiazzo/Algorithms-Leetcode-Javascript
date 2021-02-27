65c3c200d660006e1b50a065155d1ec5bfa72b0f
ignacio chiazzo cardarello <ignaciochiazzopersonal@ignacios-MacBook-Pro-2.local>
2021-02-26 21:45:34 -0500

    Duplicate emails

---
+++ LeetcodeProblems/Databases/Duplicate_emails.sql
@@ -0,0 +1,24 @@
+/*
+Easy
+
+Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+*/
+SELECT Email
+FROM Person p
+group by Email
+having count(Email) > 1
