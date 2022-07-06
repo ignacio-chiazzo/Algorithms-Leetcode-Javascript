062fc88b65e796626094e700400d765257a601e4
Ignacio Chiazzo Cardarello <ignaciochiazzo@gmail.com>
2022-07-05 21:43:10 -0400

    Update Minimum_Window_Substring.js

--- LeetcodeProblems/Algorithms/Minimum_Window_Substring.js
+++ LeetcodeProblems/Algorithms/Minimum_Window_Substring.js
@@ -15,6 +15,7 @@ If there is no such window in S that covers all characters in T, return the empt
 If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
 */
 
+// Solution 1
 var minWindow = function (s, t) {
   if (t.length === 0 || s.length < t.length) return "";
 
@@ -66,5 +67,65 @@ var getHash = function (t) {
   }
   return hash;
 };
+
+
+
+// Solution 2
+// Similar idea code slightly different;
+var buildHash = function(t) {
+    let hash = {};
+    let occ = 0;
+    for(var i = 0; i < t.length; i++) {
+        occ = hash[t[i]] == undefined ? 0 : hash[t[i]];
+        hash[t[i]] = occ + 1;
+    }
+    return hash;
+}
+
+var minWindow2 = function(s, t) {
+    var hashT = buildHash(t);
+    var start = 0; 
+    var end = 0;
+    var countLeft = t.length;
+    var minWindow = "";
+    var minWindowLeft = -1;
+    var maxWindowRight = -1;
+
+    while(start < s.length && end < s.length) {
+        if(countLeft > 0) { // window does not contain all elements            
+            if(hashT[s[end]] !== undefined) {
+                hashT[s[end]] = hashT[s[end]] - 1;
+                if(hashT[s[end]] >= 0) {
+                    countLeft--;
+                }
+            }
+            
+            if(countLeft > 0) {
+                end++;
+            }
+        } else { // found window
+            if(minWindowLeft == -1 || ((maxWindowRight - minWindowLeft + 1) > (end - start + 1)) ) {
+                minWindowLeft = start;
+                maxWindowRight = end;
+            }
+           if(hashT[s[start]] !== undefined) {
+               hashT[s[start]] = hashT[s[start]] + 1;
+               if(hashT[s[start]] > 0) {
+                   countLeft++;
+                   end++;
+               }
+           }
+           start++;
+        }
+    }
+    
+    if(minWindowLeft > -1) {
+        return s.substring(minWindowLeft, maxWindowRight + 1);
+    }
+    
+    return "";
+};
+
+
 
 module.exports.minWindow = minWindow;
