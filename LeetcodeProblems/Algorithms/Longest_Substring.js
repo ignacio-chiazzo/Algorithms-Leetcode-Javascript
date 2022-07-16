13cd84f2bdb4d91b2eaa07d71e7b8bd3d5111c7e
ignacio-chiazzo <ignaciochiazzo@gmail.com>
2022-07-16 10:53:41 -0400

    Added Minimum Size Subarray problem

--- LeetcodeProblems/Algorithms/Longest_Substring.js
+++ LeetcodeProblems/Algorithms/Longest_Substring.js
@@ -1,4 +1,7 @@
 /*
+Longest Substring Without Repeating Characters
+https://leetcode.com/problems/longest-substring-without-repeating-characters
+
 Given a string s, find the length of the longest substring without repeating characters.
 
 Example 1:
---
+++ LeetcodeProblems/RESOURCES.md
@@ -0,0 +1,2 @@
+[Common Patters on Leetcode problems]https://iorilan.medium.com/after-solved-1000-medium-leetcode-found-these-patterns-sliding-window-2-pointer-string-18332ca4861
+
