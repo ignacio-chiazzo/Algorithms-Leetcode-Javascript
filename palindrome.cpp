0b0103b6f51a87b9dc70e8ff427f5b9b361e3f1b
Monika Basene <113535275+Monika3002@users.noreply.github.com>
2022-10-07 19:19:08 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 
 using namespace std;
+  
+  void case1(int x)
+     {     x=121;
+         cout<<"for positive integers : "<<x<<endl;
+     }
+  void case2(int x)
+     {    x=-121;
+         cout<<"for negative integers : "<<x<<endl;
+     }
+  void case3(int x)
+     {   x=10;
+         cout<<"for integers end with zero: "<<x<<endl;
+     }
 
     bool isPalindrome(int x) {
        if(x < 0)
@@ -22,12 +35,24 @@ using namespace std;
     }
      return false;
 }
+
     int main()
     {    
-       int x=161;
-       int c=isPalindrome(x);
+       int x,c,n;
+       case1(x);
+        // cout<<isPalindrome(x);
        if(c==0)
-       {cout<<"false";}
-       else{cout<<"true";}
+       {cout<<"false"<<endl;}
+       else{cout<<"true"<<endl;}
+       case2(x);
+        c=isPalindrome(x);
+       if(c==0)
+       {cout<<"false"<<endl;}
+       else{cout<<"true"<<endl;}
+       case3(x);
+        c=isPalindrome(x);
+       if(c==0)
+       {cout<<"false"<<endl;}
+       else{cout<<"true"<<endl;}
         return 0;
     }
