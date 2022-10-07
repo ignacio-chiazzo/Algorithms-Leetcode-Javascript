7d77208497f1a0a54340443e0dbc2f63efa86faa
anonimak <sanonimak@gmail.com>
2022-10-07 13:28:40 +0700

    Added Shuffle_String problem solution

---
+++ LeetcodeProblems/Algorithms/Shuffle_String.js
@@ -0,0 +1,33 @@
+/**
+ You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+ 
+
+Example 1:
+
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+Example 2:
+
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+
+ */
+
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let arrshuffle = [];
+    indices.forEach((sindex, index) => arrshuffle[sindex] = s.charAt(index))
+    return arrshuffle.join('')
+};
+
+module.exports.restoreString = restoreString;
