426fb544428e509ea368345fca199ce8e8fd9423
Chaitra Bhat <79207846+Chaitra-Bhat383@users.noreply.github.com>
2022-10-23 14:11:46 +0530

    EsLint and Tests fix

---
+++ LeetcodeProblemsTests/Algorithms/K_Closest_Points_to_Origin_Test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+var kClosest = require('../../LeetcodeProblems/Algorithms/K_Closest_Points_to_Origin').kClosest;
+
+function test1() {
+  var points = [[1,3],[-2,2]]
+  var output = [[-2,2]]
+  assert.strictEqual(kClosest(points,1), output);
+}
+
+function test2() {
+  var points = [[3,3],[5,-1],[-2,4]]
+  var output = [[-2,4],[3,3]]
+  assert.strictEqual(kClosest(points,2), output);
+}
+
+function test() {
+  test1();
+  test2();
+}
+
+module.exports.test = test
+
