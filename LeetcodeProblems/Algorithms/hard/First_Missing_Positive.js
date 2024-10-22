cd335dc02a8555a6469ee432f4d407a3c3b51cb7
mukul <chh@adobe.com>
2024-10-22 20:04:31 +0530

    41. First Missing Positive

--- LeetcodeProblems/Algorithms/hard/First_Missing_Positive.js
+++ LeetcodeProblems/Algorithms/hard/First_Missing_Positive.js
@@ -35,17 +35,17 @@ If all elements are in their correct positions, the function returns the next po
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-    const n = nums.length
+  const n = nums.length
 
-    for (let i = 0; i < n; i++)
-        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i])
-            [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]]
+  for (let i = 0; i < n; i++)
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i])
+      [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]]
 
-    for (let i = 0; i < n; i++)
-        if (nums[i] !== i + 1)
-            return i + 1
+  for (let i = 0; i < n; i++)
+    if (nums[i] !== i + 1)
+      return i + 1
 
-    return n + 1
+  return n + 1
 };
 
 module.exports.firstMissingPositive = firstMissingPositive;
